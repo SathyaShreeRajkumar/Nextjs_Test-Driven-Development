ef1658aaf9f55f0c0a767557c65a757c0f527d81
Sathya_CES <sathya.shree@cesltd.com>
2024-05-16 13:50:02 +0530

    Updated Review comments

--- nextjs_tdd/__tests__/unit/_app/layout.test.tsx
+++ nextjs_tdd/__tests__/unit/_app/layout.test.tsx
@@ -4,7 +4,7 @@ import { render, screen } from '@testing-library/react'
 import { mockChildrenProps } from '../../__fixtures__'
 
 describe('RootLayout', () => {
-    test('should render children, home, and airlines properly', async () => {
+    test('should render children, home, and airlines properly', () => {
         const mockProps = {
             children: <div>{mockChildrenProps.children}</div>,
             home: <div>{mockChildrenProps.home}</div>,
@@ -13,9 +13,9 @@ describe('RootLayout', () => {
 
         render(<RootLayout {...mockProps} />)
 
-        const childrenProp = await screen.findByText(mockChildrenProps.children)
-        const homeProp = await screen.findByText(mockChildrenProps.home)
-        const airlinesProp = await screen.findByText(mockChildrenProps.airlines)
+        const childrenProp =  screen.getByText(mockChildrenProps.children)
+        const homeProp =  screen.getByText(mockChildrenProps.home)
+        const airlinesProp =  screen.getByText(mockChildrenProps.airlines)
 
         expect(childrenProp).toBeInTheDocument()
         expect(homeProp).toBeInTheDocument()
